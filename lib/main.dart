138c562620f6189b6d694c7c1ba7efe4b7cf86f7
Sandor Dargo <sandordargo@users.noreply.github.com>
2018-06-24 01:18:09 +0200

    feature: Replace Export FlatButton with IconButton

--- lib/main.dart
+++ lib/main.dart
@@ -181,8 +181,10 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
         actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.cloud_download), onPressed: import),
-          new FlatButton(onPressed: exportData, child: new Text("Export data")),
+          new IconButton(
+              icon: new Icon(Icons.cloud_download), onPressed: import),
+          new IconButton(
+              icon: new Icon(Icons.cloud_upload), onPressed: exportData),
           new FlatButton(onPressed: stats, child: new Text("Stats"))
         ],
       ),
